cc7dc9730e12a615bf7777e30c452f990a3e7c12
Benjamin Mosior <bemosior@gmail.com>
2015-08-01 21:45:58 -0400

    Basic layout, create page.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                @yield('content')
+            </div>
+        </div>
+
+        <!-- Latest compiled and minified JS -->
+        <script src="//code.jquery.com/jquery.js"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
