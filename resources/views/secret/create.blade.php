a06edf1a243a9f452d5702e4fef65e35d2976460
Benjamin Mosior <bemosior@gmail.com>
2015-08-03 05:22:01 -0400

    Date/time fields reversed, tabs to spaces.

--- resources/views/secret/create.blade.php
+++ resources/views/secret/create.blade.php
@@ -3,27 +3,28 @@
 @section('title', 'Create')
 
 @section('content')
-	<legend>Create Shared Secret</legend>
-	<form action="store" method="POST" role="form">
-	    <div class="form-group">
-	        <label for="secret">Secret</label>
-	        <textarea name="secret" id="secret" class="form-control" rows="3" required="required"></textarea>
-	    </div>
+    <legend>Create Shared Secret</legend>
+    <form action="store" method="POST" role="form">
+        <div class="form-group">
+            <label for="secret">Secret</label>
+            <textarea name="secret" id="secret" class="form-control" rows="3" required="required"></textarea>
+        </div>
 
-	    <div class="form-group">
-	        <label for="expires_time">Expiration (UTC)</label>
-	        <fieldset class="form-inline">
-		        <input type="time" name="expires_time" id="expires_time" class="form-control" value="{{ $now->format('H:i') }}" required="required" title="">
-		        <input type="date" name="expires_date" id="expires_date" class="form-control" value="{{ $now->format('Y-m-d') }}" required="required" title="">
-		    </fieldset>
-		</div>
+        <div class="form-group">
+            <label for="expires_time">Expiration (UTC)</label>
+            <fieldset class="form-inline">
+                <input type="date" name="expires_date" id="expires_date" class="form-control" value="{{ $now->format('Y-m-d') }}" required="required" title="">
+                <input type="time" name="expires_time" id="expires_time" class="form-control" value="{{ $now->format('H:i') }}" required="required" title="">
+                
+            </fieldset>
+        </div>
 
-		<div class="form-group">
-		    <label for="expires_views">View Limit</label>
-		    <input type="number" name="expires_views" id="expires_views" class="form-control" value="5" min="1" max="" step="" required="required" title="">
-	    </div>
+        <div class="form-group">
+            <label for="expires_views">View Limit</label>
+            <input type="number" name="expires_views" id="expires_views" class="form-control" value="5" min="1" max="" step="" required="required" title="">
+        </div>
 
-	    <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-	    <button type="submit" class="btn btn-primary">Submit</button>
-	</form>
+        <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 @stop
