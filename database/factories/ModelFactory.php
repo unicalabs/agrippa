585a842a44f593dc3ee8b4188929cbfb03a6564b
Benjamin Mosior <bemosior@gmail.com>
2015-08-07 04:41:45 -0400

    Secret factory uses correct UUID tostring.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,7 +17,7 @@ use Rhumsaa\Uuid\Uuid;
 $factory->define(App\Secret::class, function ($faker) {
     return [
         'secret' => Crypt::encrypt($faker->word(32)),
-        'uuid4' => Hash::make(Uuid::uuid4()),
+        'uuid4' => Hash::make(Uuid::uuid4()->toString()),
         'created_at' => Carbon::now(),
         'expires_at' => $faker->dateTimeBetween('+5 minutes', '+5 days'),
         'expires_views' => $faker->numberBetween(5,1000),
