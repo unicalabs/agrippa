7f669a1df6fce54c7a71a79b681396adbbd8c7b6
Benjamin Mosior <bemosior@gmail.com>
2018-04-12 23:56:18 -0400

    Assume https.

--- resources/views/secret/store.blade.php
+++ resources/views/secret/store.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <legend>Shared Secret</legend>
-    <pre>{{ Request::root() }}/show/{{ $uuid4 }}</pre>
+    <pre>https://{{ Request::getHost() }}/show/{{ $uuid4 }}</pre>
     <span class="label label-default">Expires <span id="datetime">{{ $expires_at }}</span> or in {{ $views_remaining }} 
         @if($views_remaining == 1)
             view.
